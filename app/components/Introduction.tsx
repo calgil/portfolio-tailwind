d538c2c233c45e7344f9c9a8091fd6fa7dd2977d
calgil <clgilbert1105@gmail.com>
2023-07-21 13:49:26 -0600

    create intro

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,7 +1,19 @@
 export const Introduction = () => {
   return (
-    <section id="introduction">
-      <h1>Introduction!</h1>
+    <section id="introduction" className="pt-48">
+      <div className="w-3/4">
+        <h1 className="text-3xl">Hi, I&apos;m Calvin!</h1>
+        <p className="mt-6 text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+          perferendis nesciunt culpa voluptatem eius magni deserunt velit quam
+          tempora ut
+        </p>
+        <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          quisquam, nesciunt commodi sapiente minus esse quis officiis itaque
+          iusto quibusdam.
+        </p>
+      </div>
     </section>
   );
 };
